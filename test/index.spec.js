3b0135540656c3ee02b0996873860fc1e88af14f
jessikalayne <jessika.layne.pereira@gmail.com>
2019-05-06 16:51:06 -0300

    change tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -10,26 +10,21 @@ describe('cpfValidator', () => {
     });
 
     it('CPF is valid', () => {
-        const CPF_TEST = '39107173814';
-        expect(cpf.cpfValidator(CPF_TEST)).to.equal(true);
+        expect(cpf.cpfValidator('39107173814')).to.equal(true);
     });
 
     it('CPF is not valid', () => {
-        const CPF_TEST = '39107173815';
-        expect(cpf.cpfValidator(CPF_TEST)).to.equal(false);
+        expect(cpf.cpfValidator('39107173815')).to.equal(false);
     });
 
     it('CPF is incomplete', () => {
-        const CPF_TEST = '3910717';
-        expect(cpf.cpfValidator(CPF_TEST)).to.equal(false);
+        expect(cpf.cpfValidator('3910717')).to.equal(false);
     });
 
     it('CPF can not be empty', () => {
-        const CPF_TEST = '';
-        expect(cpf.cpfValidator(CPF_TEST)).to.equal(false);
+        expect(cpf.cpfValidator('')).to.equal(false);
     });
     it('CPF is valid', () => {
-        const CPF_TEST = '10726985368';
-        expect(cpf.cpfValidator(CPF_TEST)).to.equal(true);
+        expect(cpf.cpfValidator('10726985368')).to.equal(true);
     });
 });
