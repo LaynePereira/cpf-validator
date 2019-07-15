9340ba36cfa7f7fde5d7d9c0513810e1d45bb0e4
jessikalayne <jessika.layne.pereira@gmail.com>
2019-07-14 23:45:04 -0300

    security update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const cpfValidator = (cpf) => {
-  cpf = cpf.split('').map(Number);
+  cpf = cpf.split('').map(Number); // transforma a string em array; transforma o array de string em números;
   return digitsValidation(cpf, 9, 10) === cpf[9] && digitsValidation(cpf, 10, 11) === cpf[10] ? true : false;
-};
+}; 
 const digitsValidation = (cpf, index, mult) => {
   cpf = cpf.slice(0, index);
   const result = cpf.reduce((acum, curr) => {
